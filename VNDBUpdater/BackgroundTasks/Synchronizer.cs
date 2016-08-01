fb93c9fc6390051b26f586aa9ef1ad5705169e1d
Onkelsam <onkelsam@gmx.de>
2016-08-01 20:46:52 +0200

    Deleted old references.

--- VNDBUpdater/BackgroundTasks/Synchronizer.cs
+++ VNDBUpdater/BackgroundTasks/Synchronizer.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Linq;
 using System.Threading.Tasks;
 using VNDBUpdater.Communication.Database;
 using VNDBUpdater.Communication.VNDB;
