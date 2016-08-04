dd6c062e15e776af915e362951240473f867dc3a
Onkelsam <onkelsam@gmx.de>
2016-08-04 13:47:59 +0200

    Updated about window.

--- VNDBUpdater/ViewModels/AboutViewModel.cs
+++ VNDBUpdater/ViewModels/AboutViewModel.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace VNDBUpdater.ViewModels
 {
     class AboutViewModel : ViewModelBase
     {
+        public AboutViewModel()
+        {
+            _Commands.AddCommand("OpenGitHubLink", ExecuteOpenGitHubLink);
+        }
+
         public string Version
         {
             get { return ((AssemblyTitleAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyTitleAttribute), false)).Title + " Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString(); }
@@ -13,6 +19,11 @@ namespace VNDBUpdater.ViewModels
         public string Copyright
         {
             get { return ((AssemblyCopyrightAttribute)Attribute.GetCustomAttribute(Assembly.GetExecutingAssembly(), typeof(AssemblyCopyrightAttribute), false)).Copyright; }
+        }
+
+        public void ExecuteOpenGitHubLink(object parameter)
+        {
+            Process.Start("https://github.com/Onkelsam/VNDBUpdater/releases");
         }
     }
 }
