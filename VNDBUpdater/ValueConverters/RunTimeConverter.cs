3f56b36ae42c0a55bbfa6ffd53fe8d75756761d2
Onkelsam <onkelsam@gmx.de>
2016-08-11 11:22:02 +0200

    Fixed runtime for VNs.

--- VNDBUpdater/ValueConverters/RunTimeConverter.cs
+++ VNDBUpdater/ValueConverters/RunTimeConverter.cs
@@ -8,10 +8,18 @@ namespace VNDBUpdater.ValueConverters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var span = (TimeSpan)value;
+
             if (value == null)
                 return new TimeSpan();
             else
-                return new DateTime(((TimeSpan)value).Ticks).ToString("HH:mm:ss");
+                return string.Format("{0} day{1}, {2} hour{3}, {4} minute{5}",
+                                      span.Days,
+                                      span.Days == 1 ? "" : "s",
+                                      span.Hours,
+                                      span.Hours == 1 ? "" : "s",
+                                      span.Minutes,
+                                      span.Minutes == 1 ? "" : "s");
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
